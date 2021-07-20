bb65a29086fc0a3687b81a1e7953db932295c050
NastyaPolischuk <86974781+NastyaPolischuk@users.noreply.github.com>
2021-07-20 18:55:19 +0300

    Add files via upload

---
+++ app/app.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import {interval, Subscription} from 'rxjs';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'task1';
+  symbols: string = ' '
+  sub: Subscription
+
+ constructor() {
+   this.sub = interval(3000).subscribe(() => this.symbols = this.randomStr())
+ }
+
+  randomStr() {
+    const random = '0123456789QWERTYUIOPLKJHGFDSAZXCVBNMqwertyuioplkjhgfdsazxcvbnm';
+    let rndStr = '';
+    for (let i = 0; i < 5; i++) {
+      const rndNumber = Math.floor(Math.random() * random.length);
+      rndStr += random.substring(rndNumber, rndNumber + 1);
+    }
+    return rndStr;
+
+  }
+
+
+  palindrome () {
+    return  this.symbols === this.symbols.split('').reverse().join('');
+ }
+
+  onlyNumber() {
+    return isNaN(Number(this.symbols))
+  }
+
+  strZero() {
+      return this.symbols.split('').some(element => Number(element) === 0)
+  }
+
+}
+
+function takeUntil(stopPlay$: any): any {
+    throw new Error('Function not implemented.');
+}
+
